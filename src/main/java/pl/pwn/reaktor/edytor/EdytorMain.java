f5f857b36bb7218f3db2060549d10f4497dc9f85
pawel <pawelmaciaszek@gmail.com>
2019-03-17 10:28:27 +0100

    start metod config

--- src/main/java/pl/pwn/reaktor/edytor/EdytorMain.java
+++ src/main/java/pl/pwn/reaktor/edytor/EdytorMain.java
@@ -1,13 +1,19 @@
 package pl.pwn.reaktor.edytor;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class EdytorMain extends Application {
 
 
     public void start(Stage primaryStage) throws Exception {
-
+        Parent root = FXMLLoader.load(getClass().getResource("/view/edytorView.fxml")); // wskazujemy nasz widok FXML
+        primaryStage.setTitle("Edytor");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
