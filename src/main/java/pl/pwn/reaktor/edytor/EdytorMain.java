4d3f21130a2dd3c02ff5771bc6a7c05876fd1b7b
pawel <pawelmaciaszek@gmail.com>
2019-03-17 09:57:58 +0100

    Initial commit

---
+++ src/main/java/pl/pwn/reaktor/edytor/EdytorMain.java
@@ -0,0 +1,21 @@
+package pl.pwn.reaktor.edytor;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class EdytorMain extends Application {
+
+
+
+
+
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+}
